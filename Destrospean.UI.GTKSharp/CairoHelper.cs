59475dbed3d4ae61509ee0a018624f70b27b23e3
CreatorAndrew <andrew.noah.mitchell@gmail.com>
2026-01-18 10:47:24 -0600

    Optimize performance for CairoHelper implementation

--- Destrospean.UI.GTKSharp/CairoHelper.cs
+++ Destrospean.UI.GTKSharp/CairoHelper.cs
@@ -7,7 +7,42 @@ namespace Destrospean.DestrospeanCASPEditor
 {
     public class CairoHelper
     {
-        class CairoHelperInternal
+        static readonly ICairoHelperInternal sSingleton = Common.Platform.IsWindows ? (ICairoHelperInternal)new CairoHelperInternalWindows() : (ICairoHelperInternal)new CairoHelperInternalOther();
+
+        class CairoHelperInternalOther : ICairoHelperInternal
+        {
+            public Context Create(Gdk.Drawable drawable)
+            {
+                return Gdk.CairoHelper.Create(drawable);
+            }
+
+            public void Rectangle(Context cr, Gdk.Rectangle rectangle)
+            {
+                Gdk.CairoHelper.Rectangle(cr, rectangle);
+            }
+
+            public void Region(Context cr, Gdk.Region region)
+            {
+                Gdk.CairoHelper.Region(cr, region);
+            }
+
+            public void SetSourceColor(Context cr, Gdk.Color color)
+            {
+                Gdk.CairoHelper.SetSourceColor(cr, color);
+            }
+
+            public void SetSourcePixbuf(Context cr, Gdk.Pixbuf pixbuf, double pixbuf_x, double pixbuf_y)
+            {
+                Gdk.CairoHelper.SetSourcePixbuf(cr, pixbuf, pixbuf_x, pixbuf_y);
+            }
+
+            public void SetSourcePixmap(Context cr, Gdk.Pixmap pixmap, double pixmap_x, double pixmap_y)
+            {
+                Gdk.CairoHelper.SetSourcePixmap(cr, pixmap, pixmap_x, pixmap_y);
+            }
+        }
+
+        class CairoHelperInternalWindows : ICairoHelperInternal
         {
             [DllImport("libgdk-win32-2.0-0.dll", CallingConvention = CallingConvention.Cdecl)]
             static extern IntPtr gdk_cairo_create(IntPtr drawable);
@@ -27,7 +62,7 @@ namespace Destrospean.DestrospeanCASPEditor
             [DllImport("libgdk-win32-2.0-0.dll", CallingConvention = CallingConvention.Cdecl)]
             static extern void gdk_cairo_set_source_pixmap(IntPtr cr, IntPtr pixmap, double pixmap_x, double pixmap_y);
 
-            public static Context Create(Gdk.Drawable drawable)
+            public Context Create(Gdk.Drawable drawable)
             {
                 var state = gdk_cairo_create(drawable == null ? IntPtr.Zero : drawable.Handle);
 #pragma warning disable 0612
@@ -35,7 +70,7 @@ namespace Destrospean.DestrospeanCASPEditor
 #pragma warning restore 0612
             }
 
-            public static void Rectangle(Context cr, Gdk.Rectangle rectangle)
+            public void Rectangle(Context cr, Gdk.Rectangle rectangle)
             {
                 var ptr = Marshaller.StructureToPtrAlloc(rectangle);
                 gdk_cairo_rectangle(cr == null ? IntPtr.Zero : cr.Handle, ptr);
@@ -43,12 +78,12 @@ namespace Destrospean.DestrospeanCASPEditor
                 Marshal.FreeHGlobal(ptr);
             }
 
-            public static void Region(Context cr, Gdk.Region region)
+            public void Region(Context cr, Gdk.Region region)
             {
                 gdk_cairo_region(cr == null ? IntPtr.Zero : cr.Handle, region == null ? IntPtr.Zero : region.Handle);
             }
 
-            public static void SetSourceColor(Context cr, Gdk.Color color)
+            public void SetSourceColor(Context cr, Gdk.Color color)
             {
                 var ptr = Marshaller.StructureToPtrAlloc(color);
                 gdk_cairo_set_source_color(cr == null ? IntPtr.Zero : cr.Handle, ptr);
@@ -56,100 +91,60 @@ namespace Destrospean.DestrospeanCASPEditor
                 Marshal.FreeHGlobal(ptr);
             }
 
-            public static void SetSourcePixbuf(Context cr, Gdk.Pixbuf pixbuf, double pixbuf_x, double pixbuf_y)
+            public void SetSourcePixbuf(Context cr, Gdk.Pixbuf pixbuf, double pixbuf_x, double pixbuf_y)
             {
                 gdk_cairo_set_source_pixbuf(cr == null ? IntPtr.Zero : cr.Handle, pixbuf == null ? IntPtr.Zero : pixbuf.Handle, pixbuf_x, pixbuf_y);
             }
 
-            public static void SetSourcePixmap(Context cr, Gdk.Pixmap pixmap, double pixmap_x, double pixmap_y)
+            public void SetSourcePixmap(Context cr, Gdk.Pixmap pixmap, double pixmap_x, double pixmap_y)
             {
                 gdk_cairo_set_source_pixmap(cr == null ? IntPtr.Zero : cr.Handle, pixmap == null ? IntPtr.Zero : pixmap.Handle, pixmap_x, pixmap_y);
             }
         }
 
-        static Context CreateExternal(Gdk.Drawable drawable)
+        interface ICairoHelperInternal
         {
-            return Gdk.CairoHelper.Create(drawable);
-        }
+            Context Create(Gdk.Drawable drawable);
 
-        static void RectangleExternal(Context cr, Gdk.Rectangle rectangle)
-        {
-            Gdk.CairoHelper.Rectangle(cr, rectangle);
-        }
+            void Rectangle(Context cr, Gdk.Rectangle rectangle);
 
-        static void RegionExternal(Context cr, Gdk.Region region)
-        {
-            Gdk.CairoHelper.Region(cr, region);
-        }
+            void Region(Context cr, Gdk.Region region);
 
-        static void SetSourceColorExternal(Context cr, Gdk.Color color)
-        {
-            Gdk.CairoHelper.SetSourceColor(cr, color);
-        }
+            void SetSourceColor(Context cr, Gdk.Color color);
 
-        static void SetSourcePixbufExternal(Context cr, Gdk.Pixbuf pixbuf, double pixbuf_x, double pixbuf_y)
-        {
-            Gdk.CairoHelper.SetSourcePixbuf(cr, pixbuf, pixbuf_x, pixbuf_y);
-        }
+            void SetSourcePixbuf(Context cr, Gdk.Pixbuf pixbuf, double pixbuf_x, double pixbuf_y);
 
-        static void SetSourcePixmapExternal(Context cr, Gdk.Pixmap pixmap, double pixmap_x, double pixmap_y)
-        {
-            Gdk.CairoHelper.SetSourcePixmap(cr, pixmap, pixmap_x, pixmap_y);
+            void SetSourcePixmap(Context cr, Gdk.Pixmap pixmap, double pixmap_x, double pixmap_y);
         }
 
         public static Context Create(Gdk.Drawable drawable)
         {
-            return Common.Platform.IsWindows ? CairoHelperInternal.Create(drawable) : CreateExternal(drawable);
+            return sSingleton.Create(drawable);
         }
 
         public static void Rectangle(Context cr, Gdk.Rectangle rectangle)
         {
-            if (Common.Platform.IsWindows)
-            {
-                CairoHelperInternal.Rectangle(cr, rectangle);
-                return;
-            }
-            RectangleExternal(cr, rectangle);
+            sSingleton.Rectangle(cr, rectangle);
         }
 
         public static void Region(Context cr, Gdk.Region region)
         {
-            if (Common.Platform.IsWindows)
-            {
-                CairoHelperInternal.Region(cr, region);
-                return;
-            }
-            RegionExternal(cr, region);
+            sSingleton.Region(cr, region);
         }
 
         public static void SetSourceColor(Context cr, Gdk.Color color)
         {
-            if (Common.Platform.IsWindows)
-            {
-                CairoHelperInternal.SetSourceColor(cr, color);
-                return;
-            }
-            SetSourceColorExternal(cr, color);
+            sSingleton.SetSourceColor(cr, color);
         }
 
         public static void SetSourcePixbuf(Context cr, Gdk.Pixbuf pixbuf, double pixbuf_x, double pixbuf_y)
         {
-            if (Common.Platform.IsWindows)
-            {
-                CairoHelperInternal.SetSourcePixbuf(cr, pixbuf, pixbuf_x, pixbuf_y);
-                return;
-            }
-            SetSourcePixbufExternal(cr, pixbuf, pixbuf_x, pixbuf_y);
+            sSingleton.SetSourcePixbuf(cr, pixbuf, pixbuf_x, pixbuf_y);
         }
 
         public static void SetSourcePixmap(Context cr, Gdk.Pixmap pixmap, double pixmap_x, double pixmap_y)
         {
-            if (Common.Platform.IsWindows)
-            {
-                CairoHelperInternal.SetSourcePixmap(cr, pixmap, pixmap_x, pixmap_y);
-                return;
-            }
-            SetSourcePixmapExternal(cr, pixmap, pixmap_x, pixmap_y);
+            sSingleton.SetSourcePixmap(cr, pixmap, pixmap_x, pixmap_y);
         }
     }
 }
