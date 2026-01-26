37774e79584ce1d326c287a24eb7a350d3c822be
CreatorAndrew <andrew.noah.mitchell@gmail.com>
2026-01-26 12:13:16 -0600

    Remove unneeded instruction

--- CASDesignerToolkit/Program.cs
+++ CASDesignerToolkit/Program.cs
@@ -20,7 +20,6 @@
                     var mainWindow = (MainWindow)MainWindow.Singleton;
                     mainWindow.CurrentPackage = s3pi.Package.Package.OpenPackage(0, args[0], true);
                     mainWindow.RefreshWidgets();
-                    mainWindow.NextState = NextStateOptions.NoUnsavedChanges;
                     mainWindow.AddFilePathToWindowTitle(args[0]);
                 }
                 Gtk.Application.Run();
