59b0075d55c8aa0bbd1084c27fe2f8c720734dba
CreatorAndrew <andrew.noah.mitchell@gmail.com>
2026-03-15 11:19:34 -0500

    Simplify code

--- Destrospean.Common/PreloadedData.cs
+++ Destrospean.Common/PreloadedData.cs
@@ -4,20 +4,16 @@ namespace Destrospean.Common
 {
     public static class PreloadedData
     {
-        public static readonly Dictionary<string, Destrospean.Common.Abstractions.CASPart> CASParts = new Dictionary<string, Destrospean.Common.Abstractions.CASPart>(System.StringComparer.InvariantCultureIgnoreCase);
+        public static readonly Dictionary<string, Abstractions.CASPart> CASParts = new Dictionary<string, Abstractions.CASPart>(System.StringComparer.InvariantCultureIgnoreCase);
 
-        public static readonly Dictionary<string, Destrospean.Common.Abstractions.GameObject> GameObjects = new Dictionary<string, Destrospean.Common.Abstractions.GameObject>(System.StringComparer.InvariantCultureIgnoreCase);
+        public static readonly Dictionary<string, Abstractions.GameObject> GameObjects = new Dictionary<string, Abstractions.GameObject>(System.StringComparer.InvariantCultureIgnoreCase);
 
-        public static readonly Dictionary<string, s3pi.GenericRCOLResource.GenericRCOLResource> FTPTs = new Dictionary<string, s3pi.GenericRCOLResource.GenericRCOLResource>(System.StringComparer.InvariantCultureIgnoreCase);
+        public static readonly Dictionary<string, s3pi.GenericRCOLResource.GenericRCOLResource> FTPTs = new Dictionary<string, s3pi.GenericRCOLResource.GenericRCOLResource>(System.StringComparer.InvariantCultureIgnoreCase),
+        LITEs = new Dictionary<string, s3pi.GenericRCOLResource.GenericRCOLResource>(System.StringComparer.InvariantCultureIgnoreCase),
+        MLODs = new Dictionary<string, s3pi.GenericRCOLResource.GenericRCOLResource>(System.StringComparer.InvariantCultureIgnoreCase),
+        MODLs = new Dictionary<string, s3pi.GenericRCOLResource.GenericRCOLResource>(System.StringComparer.InvariantCultureIgnoreCase),
+        VPXYs = new Dictionary<string, s3pi.GenericRCOLResource.GenericRCOLResource>(System.StringComparer.InvariantCultureIgnoreCase);
 
         public static readonly Dictionary<string, CmarNYCBorrowed.GEOM> GEOMs = new Dictionary<string, CmarNYCBorrowed.GEOM>(System.StringComparer.InvariantCultureIgnoreCase);
-
-        public static readonly Dictionary<string, s3pi.GenericRCOLResource.GenericRCOLResource> LITEs = new Dictionary<string, s3pi.GenericRCOLResource.GenericRCOLResource>(System.StringComparer.InvariantCultureIgnoreCase);
-
-        public static readonly Dictionary<string, s3pi.GenericRCOLResource.GenericRCOLResource> MLODs = new Dictionary<string, s3pi.GenericRCOLResource.GenericRCOLResource>(System.StringComparer.InvariantCultureIgnoreCase);
-
-        public static readonly Dictionary<string, s3pi.GenericRCOLResource.GenericRCOLResource> MODLs = new Dictionary<string, s3pi.GenericRCOLResource.GenericRCOLResource>(System.StringComparer.InvariantCultureIgnoreCase);
-
-        public static readonly Dictionary<string, s3pi.GenericRCOLResource.GenericRCOLResource> VPXYs = new Dictionary<string, s3pi.GenericRCOLResource.GenericRCOLResource>(System.StringComparer.InvariantCultureIgnoreCase);
     }
 }
