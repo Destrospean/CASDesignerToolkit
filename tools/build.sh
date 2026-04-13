e30cc5e7c822ac446365fa205a0673255ecdd2a7
CreatorAndrew <andrew.noah.mitchell@gmail.com>
2026-04-13 10:12:25 -0500

    Fix build scripts

--- tools/build.sh
+++ tools/build.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 cd "${0%/*}"
-docker run --rm --name casdesignertoolkit-build -v "$(pwd)/..":/CASDesignerToolkit $IMAGE_NAME bash -c "/CASDesignerToolkit/tools/_build.sh"
-rm ../CASDesignerToolkit/bin/Release/*.pdb ../CASDesignerToolkit/bin/Release/*.xml
-cp ../GameFolders.xml ../CASDesignerToolkit/bin/Release
+docker run --rm --name casdesignertoolkit-build -v "$(pwd)/..":/CASDesignerToolkit $IMAGE_NAME bash -c "/CASDesignerToolkit/tools/_build.sh && cd /CASDesignerToolkit/CASDesignerToolkit/bin/Release/ && rm *.pdb *.xml && cp /CASDesignerToolkit/GameFolders.xml ."
